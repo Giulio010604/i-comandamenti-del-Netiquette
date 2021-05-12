7fae64996bb23c85f972973209caa5db18bef1b6
stefanoghiotti <80244678+stefanoghiotti@users.noreply.github.com>
2021-05-12 18:08:35 +0200

    Netiquette 4.0 12/5/2021
    Modifica effettuata al sito con aggiunta di formattazione migliore da w3school navbars. Inizio dei collegamenti per le altre pagine del sito. Sistemazione di alcuni bug e difetti.

--- javascript.js
+++ javascript.js
@@ -16,6 +16,19 @@ window.onclick = function(event) {
     }
   }
 }
-function alert(){
-  $(".alert").alert('close')
+function showResult(str) {
+  if (str.length==0) {
+    document.getElementById("livesearch").innerHTML="";
+    document.getElementById("livesearch").style.border="0px";
+    return;
+  }
+  var xmlhttp=new XMLHttpRequest();
+  xmlhttp.onreadystatechange=function() {
+    if (this.readyState==4 && this.status==200) {
+      document.getElementById("livesearch").innerHTML=this.responseText;
+      document.getElementById("livesearch").style.border="1px solid #A5ACB2";
+    }
+  }
+  xmlhttp.open("GET","livesearch.php?q="+str,true);
+  xmlhttp.send();
 }
