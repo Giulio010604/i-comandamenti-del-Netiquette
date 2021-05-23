53606a531046a23a28a253c040e24a0229eb8d1c
stefanoghiotti <80244678+stefanoghiotti@users.noreply.github.com>
2021-05-23 11:07:50 +0200

    Netiquette 5.2.1 (TERMINATO)
    Nota di Rilascio
    La struttura del sito è organizzata in cartelle e sottocartelle per permettere una miglior praticità. Buona Lettura!

--- i-comandamenti-del-Netiquette-main/java/javascript.js
+++ i-comandamenti-del-Netiquette-main/java/javascript.js
@@ -29,6 +29,6 @@ function showResult(str) {
       document.getElementById("livesearch").style.border="1px solid #A5ACB2";
     }
   }
-  xmlhttp.open("GET","livesearch.php?q="+str,true);
+  xmlhttp.open("GET","search/livesearch.php?q="+str,true);
   xmlhttp.send();
 }
--- i-comandamenti-del-Netiquette-main/search/livesearch.php
+++ i-comandamenti-del-Netiquette-main/search/livesearch.php
@@ -43,4 +43,4 @@ if ($hint=="") {
 
 //output the response
 echo $response;
-?> 
+?>
